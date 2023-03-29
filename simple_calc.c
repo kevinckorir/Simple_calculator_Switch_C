f92175a226f3f9b779e4a40737d2e8412fe3fe49
Kevin Korir <korirckevin@gmail.com>
2023-03-29 11:20:25 +0300

    Added source code file simple_calc.c

---
+++ simple_calc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/*#include <conio.h>*/
+
+/**
+ * main - do simple calculations
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int a, b;
+	int sum, sub, mul, div;
+	char operator;
+
+	/*take input from user*/
+	printf("Enter two operands:");
+	scanf("%d %d", &a, &b);
+	printf("Enter operator:");
+	scanf(" %c", &operator);
+
+	/*Perform the operation*/
+	switch (operator)
+	{
+	case '+' : 
+		sum = a + b;
+		printf("Answer = %d\n", sum);
+		break;
+	case '-' : 
+		sub = a - b;
+		printf("Answer = %d\n", sub);
+		break;
+	case '*' :
+	       	mul = a * b;
+		printf("Answer = %d\n", mul);
+		break;
+	case '/' : div = a / b;
+		printf("Answer = %d\n", div);
+		break;
+	default:
+		printf("Enter a valid operator");
+	}
+	/*getch();*/
+	return (0);
+}
